a090f510b439fa7c875ec1042f7d547052fb9ddc
nick <nickhou@etekcity.com.cn>
2021-12-19 16:32:27 +0800

    【develop】【up】练习代码

--- app/src/main/java/com/vesync/wanandroid/ComposeActivity.kt
+++ app/src/main/java/com/vesync/wanandroid/ComposeActivity.kt
@@ -6,8 +6,6 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.OutlinedButton
@@ -24,6 +22,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.vesync.wanandroid.ui.BasicsCodelabTheme
+import com.vesync.wanandroid.ui.Greetings
 
 class ComposeActivity : ComponentActivity() {
 
@@ -40,49 +39,7 @@ class ComposeActivity : ComponentActivity() {
         }
     }
 
-
     data class Message(val author: String, val body: String)
-
-
-    @Composable
-    private fun Greetings(names: List<String> = List(1000) { "$it" }) {
-        LazyColumn(modifier = Modifier.padding(vertical = 4.dp)) {
-            items(items = names) { name ->
-                Greeting(name = name)
-            }
-        }
-    }
-
-    @Composable
-    private fun Greeting(name: String) {
-
-        val expanded = remember { mutableStateOf(false) }
-
-        val extraPadding = if (expanded.value) 48.dp else 0.dp
-
-        Surface(
-            color = MaterialTheme.colors.primary,
-            modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
-        ) {
-            Row(modifier = Modifier.padding(24.dp)) {
-                Column(
-                    modifier = Modifier
-                        .weight(1f)
-                        .padding(bottom = extraPadding)
-                ) {
-                    Text(text = "Hello, ")
-                    Text(text = name)
-                }
-                OutlinedButton(
-                    onClick = { expanded.value = !expanded.value }
-                ) {
-                    Text(if (expanded.value) "Show less" else "Show more")
-                }
-
-            }
-        }
-    }
-
 
     @Composable
     fun MessageCard(message: Message) {
@@ -114,10 +71,7 @@ class ComposeActivity : ComponentActivity() {
                 ) {
                     Text(if (expanded.value) "Show less" else "Show more")
                 }
-
                 Greetings()
-
-
             }
 
 
---
+++ app/src/main/java/com/vesync/wanandroid/ui/MessageCard.kt
@@ -0,0 +1,67 @@
+package com.vesync.wanandroid.ui
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedButton
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.vesync.wanandroid.R
+
+/**
+ * Author: Nickhou
+ * Date: 2021/12/19 15:25
+ * Description: 组件
+ * History:
+ * <author> <time> <version> <desc>
+ * Nickhou 2021/12/19 1.0 首次创建
+ */
+@Preview
+@Composable
+fun Greetings(names: List<String> = List(1000) { "$it" }) {
+    LazyColumn(modifier = Modifier.padding(vertical = 4.dp)) {
+        items(items = names) { name ->
+            Greeting(name = name)
+        }
+    }
+}
+
+@Composable
+private fun Greeting(name: String) {
+
+    val expanded = remember { mutableStateOf(false) }
+
+    val extraPadding = if (expanded.value) 48.dp else 0.dp
+
+    Surface(
+        color = MaterialTheme.colors.primary,
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
+        Row(modifier = Modifier.padding(24.dp)) {
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(bottom = extraPadding)
+            ) {
+                Text(text = "Hello, ")
+                Text(text = name)
+            }
+            OutlinedButton(
+                onClick = { expanded.value = !expanded.value }
+            ) {
+                Text(if (expanded.value) stringResource(R.string.show_less) else stringResource(R.string.show_more))
+            }
+
+        }
+    }
+}
